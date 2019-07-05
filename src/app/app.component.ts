6c56f7ad278df45e551a8f362344b0bd3492a878
Sebastien-Lechat <seb_le@hotmail.fr>
2019-07-05 16:11:56 +0200

    Vendredi 05/07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
+    // {
+    //   title: 'Home',
+    //   url: '/home',
+    //   icon: 'home'
+    // },
     {
       title: 'Profil',
       url: '/profil',
@@ -29,11 +29,6 @@ export class AppComponent {
       title: 'Ma liste de poste',
       url: '/list',
       icon: 'list'
-    },
-    {
-      title: 'Description du poste',
-      url: '/job',
-      icon: 'school'
     }
   ];
 
